52fc50c4876a89178c5825155f482e5bfbb75bfa
Priestly Patrick bassey <pbassey30@gmail.com>
2025-06-03 18:22:41 +0100

    made changes to the baseURl

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
 export const AxiosInstance = axios.create({
-    baseURL: "https://huntamazonfinds2-umgzkdi8.b4a.run/"
+    baseURL: "https://hunt-amazon-finds-back-end.onrender.com"
 })
 
 // http://localhost:5001/
 // https://huntamazonfinds2-umgzkdi8.b4a.run/
+// https://hunt-amazon-finds-back-end.onrender.com
